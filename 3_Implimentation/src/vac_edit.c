29fe90558b9119ab1b2d28d0f670ad2ab8679daa
Bommisetty Janaki Sruthi <62583721+12157@users.noreply.github.com>
2022-02-10 17:39:52 +0530

    Update vac_edit.c

--- 3_Implimentation/src/vac_edit.c
+++ 3_Implimentation/src/vac_edit.c
@@ -1,11 +1,11 @@
 /**
  * @file vac_edit.c
- * @author your name (you@domain.com)
- * @brief used to edit the record in database for vacination
+ *  @author your name (Bommisetty Janaki Sruthi)
+ * @brief used to delete record from database
  * @version 0.1
- * @date 2021-04-15
+ * @date 2022-02-08
  * 
- * @copyright Copyright (c) 2021
+ * @copyright Copyright (c) 2022
  * 
  */
 //HEADER FILES
