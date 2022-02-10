79f5a9b6943b2f7889f776bad7d3bef5fbff4178
Bommisetty Janaki Sruthi <62583721+12157@users.noreply.github.com>
2022-02-10 17:33:48 +0530

    Update LoginScreen.c

--- 3_Implimentation/src/LoginScreen.c
+++ 3_Implimentation/src/LoginScreen.c
@@ -1,12 +1,11 @@
 /**
  * @file LoginScreen.c
- * @author your name (you@domain.com)
- * @brief creates a login screen for the  program
+ * @author your name (Bommisetty Janaki Sruthi)
+ * @brief used to delete record from database
  * @version 0.1
- * @date 2021-04-15
+ * @date 2022-02-08
  * 
- * @copyright Copyright (c) 2021
- * 
+ * @copyright Copyright (c) 2022
  */
 //HEADER FILES
 #include<stdio.h>//Use for standard I/O Operation
