d19436afaa6aa9cdd4477c4e7cc0c0fb458cbeaf
Bommisetty Janaki Sruthi <62583721+12157@users.noreply.github.com>
2022-02-11 11:45:18 +0530

    Update ex_it.c

--- 3_Implememtation/src/ex_it.c
+++ 3_Implememtation/src/ex_it.c
@@ -1,11 +1,12 @@
 /**
  * @file ex_it.c
- * @author your name (you@domain.com)
- * @brief used to exit the program
+*/@file covid_management.h
+ * @author your name (sruthibommisetty3@gmail.com)
+ * @brief the program is called covid management system
  * @version 0.1
- * @date 2021-04-15
+ * @date 2022-02-08
  * 
- * @copyright Copyright (c) 2021
+ * @copyright Copyright (c) 2022
  * 
  */
 //HEADER FILES
